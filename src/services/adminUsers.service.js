07b8867f09acf864b6f500034130d79e47d565ba
Ulis11 <ulig11mx@gmail.com>
2026-06-10 01:06:48 -0600

    quickfix

--- src/services/adminUsers.service.js
+++ src/services/adminUsers.service.js
@@ -111,7 +111,7 @@ async function createUser(body) {
     return {
       ok: false,
       status: 400,
-      message: `Password es requerida y debe tener al menos ${MIN_PASSWORD_LENGTH} caracteres`,
+      message: `Contraseña es requerida y debe tener al menos ${MIN_PASSWORD_LENGTH} caracteres`,
     };
   }
 
